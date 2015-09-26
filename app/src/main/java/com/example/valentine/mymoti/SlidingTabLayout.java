3fa269408499eb1463e61927fe693535a7d2a4cf
valentinerutto <rutto28@gmail.com>
2015-09-26 06:39:33 +0300

    populated TabStrip

--- app/src/main/java/com/example/valentine/mymoti/SlidingTabLayout.java
+++ app/src/main/java/com/example/valentine/mymoti/SlidingTabLayout.java
@@ -2,9 +2,13 @@ package com.example.valentine.mymoti;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.SparseArray;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
