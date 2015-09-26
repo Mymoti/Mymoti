16db34d398f3e5cf9178face6752b8556005abef
valentinerutto <rutto28@gmail.com>
2015-09-26 07:29:27 +0300

    added  the tabs layout files to the fragments

---
+++ app/src/main/java/com/example/valentine/mymoti/Tab2.java
@@ -0,0 +1,19 @@
+package com.example.valentine.mymoti;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by valentine on 9/26/15.
+ */
+public class Tab2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_2,container,false);
+        return v;
+    }
+}
